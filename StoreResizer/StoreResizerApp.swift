9c5429ef9906c652c21c2b3466ad9ec1c01fb14e
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2026-01-18 20:11:00 +0300

    Initial Commit

---
+++ StoreResizer/StoreResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreResizerApp.swift
+//  StoreResizer
+//
+//  Created by Akinalp Fidan on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct StoreResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
